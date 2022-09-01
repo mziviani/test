eaab3e1f8218704d528411381abec48d80499b05
mziviani <zivianimatteo@gmail.com>
2022-09-01 09:16:12 +0200

    Aggiorna main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
-basic.showString("Ciao Mondo")
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+})
 basic.forever(function () {
-    basic.showString("Ciao Matteo")
+	
 })
